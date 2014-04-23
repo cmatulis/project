bce9c27f712c25593332798a8ec9a0965ec836a2
Sojung Lee <slee14@wellesley.edu>
2014-04-22 23:09:26 -0400

    signuppage

--- blog-functions.php
+++ blog-functions.php
@@ -148,7 +148,7 @@ function printPageHeader() {
       <div class="container">
         <h1>Welcome!</h1>
         <p>Poster is a blogging website.  Click below to sign up.</p>
-        <p><a class="btn btn-primary btn-lg" role="button">Sign Up &raquo;</a></p>
+        <p><a href="signUpPage.php" class="btn btn-primary btn-lg" role="button">Sign Up</a>
       </div>
     </div>
 EOT;
@@ -907,5 +907,54 @@ EOT;
 
 }
 
+function printSignupPage(){
+  print <<<EOT
+  <!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="shortcut icon" href="../../assets/ico/favicon.ico">
+
+    <title>Sign Up</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="bootstrap-3.1.1-dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="bootstrap-3.1.1-dist/css/signin.css" rel="stylesheet"> 
+
+    <!-- Just for debugging purposes. Don't actually copy this line! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+
+    <div class="container">
+      <form class="form-signin" method ="post" action = "signUpPage.php" role="form>
+        <h2 class="form-signin-heading">Sign Up</h2>
+        <input type="username" name="username" class="form-control" placeholder="Username" required autofocus>
+        <input type="email" name="email" class="form-control" placeholder="Email address" required>
+        <input type="password" name="password" class="form-control" placeholder="Password" required>
+        <p><button type="submit" name="submit" href="signUpSuccess.php" class="btn btn-lg btn-primary btn-block">Sign up</button><p>
+        
+      </form>
+    </div> <!-- /container -->
+ 
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+
+EOT;
+}
 
 ?>
